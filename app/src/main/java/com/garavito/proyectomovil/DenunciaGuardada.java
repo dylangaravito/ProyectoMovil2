682799c498182d09f2795032d58bd71e983e9717
Dylan Garavito <dilan_garavito@hotmail.com>
2021-05-30 14:31:33 -0500

    Initial commit

---
+++ app/src/main/java/com/garavito/proyectomovil/DenunciaGuardada.java
@@ -0,0 +1,14 @@
+package com.garavito.proyectomovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DenunciaGuardada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_denuncia_guardada);
+    }
+}
---
+++ app/src/main/java/com/garavito/proyectomovil/NuevaDenuncia2.java
@@ -0,0 +1,14 @@
+package com.garavito.proyectomovil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NuevaDenuncia2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_denuncia2);
+    }
+}
